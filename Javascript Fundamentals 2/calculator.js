b02c5455b1d06008225265226f7f40faf2ec1227
Seth McKnight <7377802+sethmcknight@users.noreply.github.com>
2025-03-09 20:04:12 +0000

    Calculate passing exam grade

--- Javascript Fundamentals 2/calculator.js
+++ Javascript Fundamentals 2/calculator.js
@@ -6,6 +6,7 @@ function drink () {
 drink();
 */
 
+/*
 let num1 = parseInt(prompt('Type in a number.'));
 let num2 = parseInt(prompt('Type in another number.'));
 
@@ -14,3 +15,24 @@ function add(x, y) {
 }
 
 alert(add(num1, num2));
+*/
+
+//Global Variables
+const passing = 75;
+
+//Functions
+function getCurrent(){
+    return parseInt(prompt('What is your current grade?'));
+};
+
+function getWeight(){
+    return parseInt(prompt('What is the weight of the exam?'));
+};
+
+function calculate(){
+    let finalScore = ((passing - (1 - (w/100)) * c) / (w/100));
+    let rounded = Math.round(finalScore);
+    return `You must get a score of ${rounded}% to pass.`;
+}
+
+alert( calculate(getCurrent(), getWeight()) );
