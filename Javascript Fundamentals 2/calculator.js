a129d43279cf8eb94d51626cca3be58d512f9c1e
Seth McKnight <7377802+sethmcknight@users.noreply.github.com>
2025-03-09 19:57:21 +0000

    refactor to move functions below all variable declarations

--- Javascript Fundamentals 2/calculator.js
+++ Javascript Fundamentals 2/calculator.js
@@ -6,14 +6,11 @@ function drink () {
 drink();
 */
 
+let num1 = parseInt(prompt('Type in a number.'));
+let num2 = parseInt(prompt('Type in another number.'));
 
 function add(x, y) {
     return x + y;
 }
-
-//add(3, 5);
-
-let num1 = parseInt(prompt('Type in a number.'));
-let num2 = parseInt(prompt('Type in another number.'));
 
 alert(add(num1, num2));
