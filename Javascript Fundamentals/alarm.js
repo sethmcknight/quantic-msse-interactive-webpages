576d0f3d24b656ab0845ee46009c458dc965454d
Seth McKnight <7377802+sethmcknight@users.noreply.github.com>
2025-03-09 19:34:13 +0000

    fix missing }

--- Javascript Fundamentals/alarm.js
+++ Javascript Fundamentals/alarm.js
@@ -1,9 +1,10 @@
 let time = 30;
 const message = "Wake up!";
 
-for (let i=0; i<=time; i++){
+for (let i = 0; i <= time; i++){
     if (i === time){
         alert(message)
     } else {
         console.log(i);
+}
 };
