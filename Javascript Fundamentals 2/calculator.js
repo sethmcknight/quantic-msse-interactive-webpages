e8fc465eb97a3ff466e8fe0348c741b38ce8d7a1
Seth McKnight <7377802+sethmcknight@users.noreply.github.com>
2025-03-09 19:48:13 +0000

    add prompts to get user inputs to add

--- Javascript Fundamentals 2/calculator.js
+++ Javascript Fundamentals 2/calculator.js
@@ -1,11 +1,22 @@
+/*
 function drink () {
     alert("Cortado");
 };
 
 drink();
+*/
+
 
 function add(x, y) {
     return x + y;
 }
 
-add(3, 5);
+//add(3, 5);
+
+prompt('Type in a number.');
+prompt('Type in another number.');
+
+let num1 = prompt('Type in a number.');
+let num2 = prompt('Type in another number.');
+
+alert(add(num1, num2));
