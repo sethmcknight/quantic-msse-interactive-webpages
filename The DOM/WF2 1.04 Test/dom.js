2091ac31d3225e060c8843ba3b359344265c75be
Seth McKnight <7377802+sethmcknight@users.noreply.github.com>
2025-03-12 03:21:38 +0000

    Lesson 4 coding challenge

--- The DOM/WF2 1.04 Test/dom.js
+++ The DOM/WF2 1.04 Test/dom.js
@@ -1,13 +1,34 @@
 const $h2 = document.querySelector('h2');
-/* query Selector by ID
+// query Selector by ID
 const $list = document.querySelector('#my-list');
-*/
+// create a variable that references the span element
+const $span = document.querySelector('span');
+
 $h2.textContent = "Ken's To Do List";
 
+let $items = $list.getElementsByTagName('li');
+
+// create global variable equal to length of items array
+let num = $items.length;
+
+update();
+
+// create the functions to update the todo list
+// function updates the number of items in the list
+function update() {
+    $span.textContent = `Number of items: ${num}`;
+    if (num === 0) {
+        alert('You have no items in your list');    
+    };
+};
+
+// function to add a new item to the list
 function addListItem(text) {
     let $item = document.createElement('li');
     $item.textContent = text;
     $list.append($item);
+    num++;
+    update();
 }
 
 addListItem("Eat");
@@ -15,7 +36,15 @@ addListItem("Eat");
 /* store all list items in an array
 let $items = $list.getElementsByTagName('li');
 */
-
+// function to remove an item from the list
 function removeItem(index) {
     $items[index].remove();
+    num--;
+    update();
+}
+// function to strike through an item in the list
+function strikeItem(index) {
+    $items[index].setAttribute('class', 'strike');
+    num--;
+    update();
 }
